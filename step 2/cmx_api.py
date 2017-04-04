352f6eb183c3e87174ebe2430751ac19a8d3c743
hghaban <hghaban@cisco.com>
2017-04-04 19:19:12 +0200

    get_active_client_count added to Class

--- step 2/cmx_api.py
+++ step 2/cmx_api.py
@@ -32,3 +32,10 @@ class cmx_api:
         parameters = {"floorrefid":floorrefid}
         response = requests.request("GET", url, headers=self.headers)
         print(response.text)
+
+
+
+    def get_active_client_count(server):
+        url = self.server+"/api/location/v2/clients/count"
+        response = requests.request("GET", url, headers=self.headers)
+        print(response.text)
