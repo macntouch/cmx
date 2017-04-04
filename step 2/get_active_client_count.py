d353098677a6a50e9d1a92189c2786e319c293e9
hghaban <hghaban@cisco.com>
2017-04-04 19:18:27 +0200

    Revert "get_active_client_count added to Class"
    This reverts commit 61e3c518f9c5ba3d61491a6d2034bd2bef3cc51c.

--- step 2/get_active_client_count.py
+++
@@ -1,7 +0,0 @@
-
-
-def get_active_client_count(server):
-
-	url = self.server+"/api/location/v2/clients/count"
-	response = requests.request("GET", url, headers=self.headers)
-	print(response.text)
