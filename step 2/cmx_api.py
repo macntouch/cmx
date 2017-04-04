e95ef76b56822fde99af635dd4325f7dc36f4819
Nabeel Rajab <nrajab@cisco.com>
2017-04-04 18:50:27 +0200

    CMX API wrapper class creation
    Initial class for CMX API wrapper. This commit contains the
    initialiser, set_headers function and history of mac address function

---
+++ step 2/cmx_api.py
@@ -0,0 +1,18 @@
+import requests
+
+class cmx_api:
+
+    def __init__(self, server, key):
+        self.server = server
+        self.headers = set_headers(key)
+
+    def set_headers(key):
+    	accessToken_hdr = 'Bearer ' + key
+    	cmx_header = {'Authorization': accessToken_hdr, 'Content-Type': 'application/json; charset=utf-8'}
+    	return (cmx_header)
+
+    def get_history_of_client_by_macaddress(server, macaddr, date):
+        url = self.server+"/api/location/v1/history/clients/"
+        parameters = {"macaddress":macaddr, "date":date}
+        response = requests.request("GET", url, params=parameters, headers=self.headers)
+        print(response.text)
