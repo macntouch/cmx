e908cd96fb64c624301758996cefec0edb49f9e5
Nabeel Rajab <nrajab@cisco.com>
2017-04-04 19:09:43 +0200

    added get_tag_by_macadress function to class

--- step 2/cmx_api.py
+++ step 2/cmx_api.py
@@ -11,17 +11,24 @@ class cmx_api:
     	cmx_header = {'Authorization': accessToken_hdr, 'Content-Type': 'application/json; charset=utf-8'}
     	return (cmx_header)
 
-    def get_history_of_client_by_macaddress(server, macaddr, date):
+    def get_history_of_client_by_macaddress(macaddr, date):
         url = self.server+"/api/location/v1/history/clients/"
         parameters = {"macaddress":macaddr, "date":date}
         response = requests.request("GET", url, params=parameters, headers=self.headers)
         print(response.text)
 
+<<<<<<< Updated upstream
     def get_site_details_by_ID_or_name():
 
         url = self.server+"/api/config/v1/sites/"
         response = requests.request("GET", url, headers=self.headers)
 
+=======
+    def get_tag_by_macadress(macaddr):
+        url = self.server+"/api/location/v1/tags/"
+        parameters = {"macaddress":macaddr}
+        response = requests.request("GET", url, params=parameters, headers=self.headers)
+>>>>>>> Stashed changes
         print(response.text)
 
     def list_of_beacons_present_on_a_floor(floorrefid):
